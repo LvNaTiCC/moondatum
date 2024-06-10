c4154e17c00f459ef7dd0998ade58b0f7facfa6e
LvNaTiC <lvnaticc@proton.me>
2024-06-10 19:09:43 +0700

    Strip unnecessary stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
@@ -16,20 +7,14 @@
 #include <GLES2/gl2.h>
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
-#ifdef __EMSCRIPTEN__
-#include "../libs/emscripten/emscripten_mainloop_stub.h"
-#endif
-
 #include <nfd.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height);
 
@@ -45,30 +30,11 @@ int main(int, char**)
     if (!glfwInit())
         return 1;
 
-    // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-    // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-    // GL 3.0 + GLSL 130
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
 
-    // Create window with graphics contextm
+
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Moondatum", nullptr, nullptr);
     if (window == nullptr)
         return 1;
@@ -123,102 +89,52 @@ int main(int, char**)
     //IM_ASSERT(font != nullptr);
 
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
-#ifdef __EMSCRIPTEN__
-    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
-    io.IniFilename = nullptr;
-    EMSCRIPTEN_MAINLOOP_BEGIN
-#else
+
     int my_image_width = 0;
     int my_image_height = 0;
     GLuint my_image_texture = 0;
     bool ret = LoadTextureFromFile("E:/images/vscbg/icons/__hatsune_miku_vocaloid_drawn_by_rrrssr__b1666488558d076a70d9fa5ed989bfc9.png", &my_image_texture, &my_image_width, &my_image_height);
     IM_ASSERT(ret);
     while (!glfwWindowShouldClose(window))
-#endif
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
-
-        // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
+
+        #if 1
+            bool show_demo_window = true;
             ImGui::ShowDemoWindow(&show_demo_window);
+        #endif
 
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        ImGui::Begin("OpenGL Texture Text", nullptr);
+        if (ImGui::Button("Change Image"))
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            nfdchar_t *outPath = NULL;
+            nfdresult_t result = NFD_OpenDialog( NULL, NULL, &outPath );
+                
+            if ( result == NFD_OKAY ) {
+                puts("Success!");
+                ret = LoadTextureFromFile(outPath, &my_image_texture, &my_image_width, &my_image_height);
+                IM_ASSERT(ret);
+                puts(outPath);
+                free(outPath);
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-
-            ImGui::Begin("OpenGL Texture Text", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
-            if (ImGui::Button("Change Image"))
-            {
-                nfdchar_t *outPath = NULL;
-                nfdresult_t result = NFD_OpenDialog( NULL, NULL, &outPath );
-                    
-                if ( result == NFD_OKAY ) {
-                    puts("Success!");
-                    ret = LoadTextureFromFile(outPath, &my_image_texture, &my_image_width, &my_image_height);
-                    IM_ASSERT(ret);
-                    puts(outPath);
-                    free(outPath);
-
-                }
-                else if ( result == NFD_CANCEL ) {
-                    puts("User pressed cancel.");
-                }
-                else {
-                    printf("Error: %s\n", NFD_GetError() );
-                }
             }
-            ImGui::Text("pointer = %d", my_image_texture);
-            ImGui::Text("size = %d x %d", my_image_width, my_image_height);
-            ImGui::Text("path = %d x %d", my_image_width, my_image_height);
-            ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
-            ImGui::End();
-
+            else if ( result == NFD_CANCEL ) {
+                puts("User pressed cancel.");
+            }
+            else {
+                printf("Error: %s\n", NFD_GetError() );
+            }
         }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        ImGui::Text("Dimensions: %d x %d", my_image_width, my_image_height);
+        ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+        ImGui::End();
 
         // Rendering
         ImGui::Render();
@@ -242,9 +158,6 @@ int main(int, char**)
 
         glfwSwapBuffers(window);
     }
-#ifdef __EMSCRIPTEN__
-    EMSCRIPTEN_MAINLOOP_END;
-#endif
 
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
@@ -256,9 +169,6 @@ int main(int, char**)
 
     return 0;
 }
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 // Simple helper function to load an image into a OpenGL texture with common settings
 bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
